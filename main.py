76fe5026b5cc61141883e99afa298630fc702f12
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-21 06:53:13 +0000

    Add default displays

--- main.py
+++ main.py
@@ -1,66 +1,14 @@
-import random
+"""
+A rock, paper, scissors game application between the system and the user.
+"""
+from image import logo
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+# Default Displays
+print(logo)
+print('Welcome, read the information below if you are not familiar with the game.')
+print('Game Information: \nRock wins against scissors.\nScissors win against paper.\nPaper wins against rock.\n')
+print('Instruction: Type 0 for Rock, 1 for Paper and 2 for Scissors\n')
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-
-print("************** Welcome to A3AJAGBE RPS Game **************\n")
-
-print("Game Information: \nRock wins against scissors.\nScissors win against paper.\nPaper wins against rock.\n")
-
-print("Instruction: Type 0 for Rock, 1 for Paper and 2 for Scissors\n")
-
-images = [rock, paper, scissors]
-
-choice = int(input("What is your choice? \n"))
-
-if choice > (len(images)-1) or choice < 0:
-  print("Invalid Input\n")
-else:
-  print(images[choice])
-
-
-print("Computer's choice: ")
-random_choice = random.randint(0,2)
-print(images[random_choice])
-
-if choice == 0 and random_choice == 2:
-  print("You win")
-elif choice == 0 and random_choice == 0:
-  print("It's a draw, Try Again")
-elif choice == 2 and random_choice == 1:
-  print("You win")
-elif choice == 2 and random_choice == 2:
-  print("It's a draw, Try Again")
-elif choice == 1 and random_choice == 0:
-  print("You win")
-elif choice == 1 and random_choice == 1:
-  print("It's a draw, Try Again")
-else:
-  print("You lose, Try Again!!!")
 
 
 
