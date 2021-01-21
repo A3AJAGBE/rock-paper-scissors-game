486b8616f7f7b0f11da295ff1611498c79794dd9
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-21 07:04:55 +0000

    Add the computer choice using random

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 A rock, paper, scissors game application between the system and the user.
 """
 from image import logo, rock, paper, scissors
+import random
 
 # Default Displays
 print(logo)
@@ -9,11 +10,11 @@ print('Welcome, read the information below if you are not familiar with the game
 print('Game Information: \nRock wins against scissors.\nScissors win against paper.\nPaper wins against rock.\n')
 print('Instruction: Type 0 for Rock, 1 for Paper and 2 for Scissors\n')
 
-# User starts the game
-choice = int(input("What hand gesture is your choice? \n"))
-
 # Add the hand gestures in a list
 images = [rock, paper, scissors]
+
+# User starts the game
+choice = int(input("What hand gesture is your choice? \n"))
 
 # Make sure the user input is valid
 if choice > (len(images) - 1) or choice < 0:
@@ -21,3 +22,7 @@ if choice > (len(images) - 1) or choice < 0:
 else:
     print(images[choice])
 
+# Computer prompt
+print("Computer's choice: ")
+random_choice = random.randint(0, 2)
+print(images[random_choice])
